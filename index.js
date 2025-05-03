2c290dab5bdd897070a8cca5b6560ab1887b98d9
Matheus Cantarutti <cantaruttim@outlook.com>
2025-05-03 11:09:09 -0300

    inital of project

---
+++ index.js
@@ -0,0 +1,14 @@
+// it has to be inside the server folder
+
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001')
+});
